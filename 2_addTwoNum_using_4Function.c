7d0cb0dfd98fc09c55721baa3cd540baf9a7b1b9
Rajshree2204 <124159993+Rajshree2204@users.noreply.github.com>
2023-02-01 23:47:31 +0530

    addition of two numbers using  functions

--- 2_addTwoNum_using_4Function.c
+++ 2_addTwoNum_using_4Function.c
@@ -5,22 +5,34 @@ float add(float x,float y){
     return(x+y);
 }
 
-void output(float* sum){
-   printf("The sum of two numder is: %.2f",*sum); 
+void output(float* sum,float* A,float* B){ 
+   printf("The sum %f and %f is: %.2f",*A,*B,*sum); 
 }
 
+float intput1(float a){
+    printf("Enter two numbers for addition\n");
+    printf("Enter the first number for addition: ");
+    scanf("%f",&a);
+    return(a);
+}
+
+float intput2(float b){
+   
+    printf("Enter the second number for addition: ");
+    scanf("%f",&b);
+    return(b);
+}
 
 int main()
 {
     float add(float x,float y); 
-    float a,b,sum;
-    printf("Enter two numbers for addition\n");
-    printf("Enter the first number for addition: ");
-    scanf("%f",&a);
-    printf("Enter the second for addition: ");
-    scanf("%f",&b);
-    sum=add(a,b);
-    void output();
-    output(&sum);
+    float A,B,sum,x,y;
+    float intput1(float a);
+    float intput2(float b);
+    A=intput1(x);
+    B=intput2(y); 
+    sum=add(A,B);
+    void output(float* sum,float* A, float* B);
+    output(&sum,&A,&B);  
     return 0; 
 }
