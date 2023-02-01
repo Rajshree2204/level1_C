8dfe831b1219ae96b5c4ad49baa7386949bc040f
Rajshree2204 <124159993+Rajshree2204@users.noreply.github.com>
2023-02-01 18:18:01 +0530

    spacing after the print statement

--- 1_addTwoNum.c
+++ 1_addTwoNum.c
@@ -2,10 +2,10 @@
 void main(){
     float a,b;
     printf("Enter two numbers for performing addition\n");
-    printf("Enter the first number for addition:");
+    printf("Enter the first number for addition: ");
     scanf("%f",&a);
-    printf("Enter the second number for addition:");
+    printf("Enter the second number for addition: ");
     scanf("%f",&b);
-    printf("The sum of %f and %f is: %.2f",a,b,a+b);
+    printf("The sum of %.2f and %.2f is: %.2f",a,b,a+b);
 } 
    
