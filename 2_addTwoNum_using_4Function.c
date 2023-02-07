c1232182141cfc622d77819b169d5cf78bde4378
Rajshree2204 <124159993+Rajshree2204@users.noreply.github.com>
2023-02-07 22:59:04 +0530

    making 1 i/p function and calling it twice

--- 2_addTwoNum_using_4Function.c
+++ 2_addTwoNum_using_4Function.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
 float intput1(float a){
-    printf("Enter two numbers for addition\n");
-    printf("Enter the first number for addition: ");
+    printf("Enter a number for addition: ");
     scanf("%f",&a);
     return(a);
-}
-
-float intput2(float b){
-   
-    printf("Enter the second number for addition: ");
-    scanf("%f",&b);
-    return(b);
 }
 
 float add(float x,float y){
@@ -19,21 +11,18 @@ float add(float x,float y){
     return(x+y);
 }
 
-void output(float* sum,float* A,float* B){ 
-   printf("The sum of %.2f and %.2f is: %.3f",*A,*B,*sum); 
+void output(float sum,float A,float B){ 
+   printf("The sum of %.2f and %.2f is: %.3f",A,B,sum); 
 }
 
 
 int main() 
 {
-    float add(float x,float y); 
     float A,B,sum,a,b;
-    float intput1(float a);
-    float intput2(float b);
-    void output(float* sum,float* A, float* B);
+    printf("Enter two numbers for addition\n");
     A=intput1(a); 
-    B=intput2(b); 
+    B=intput1(b); 
     sum=add(A,B);
-    output(&sum,&A,&B);  
+    output(sum,A,B);  
     return 0; 
 }
